921c5add61c4903d8cf9bc97bbaecabe6fb5082d
theguywhosaspy-ux <theguywhosaspy@gmail.com>
2026-04-27 22:51:07 -0400

    removed useless info

--- udp _listener.py
+++ udp _listener.py
@@ -5,17 +5,15 @@ from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 MCAST_GRP = '239.255.0.1'
 MCAST_PORT = 5007
 
-# 1. Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-# 2. Allow multiple listeners on the same port
+
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# 3. Bind to the server address
-# On Windows, bind to the multicast address; on Linux, often bind to '' (all interfaces)
+
 sock.bind(('', MCAST_PORT))
 
-# 4. Join the multicast group
+
 mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
@@ -32,7 +30,7 @@ while True:
         
         with open("chacha.key", "rb") as key_file:
             loaded_key = key_file.read()
-        print(len(loaded_key))
+            
         chacha = ChaCha20Poly1305(loaded_key)
         nonce = data[:12]
         ci_text = data[12:]
